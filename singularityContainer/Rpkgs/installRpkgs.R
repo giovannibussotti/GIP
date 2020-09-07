eb9257d4351a335827902be0ab0dcbecc8da62dd
Giovanni Bussotti <giovannibussotti@gmail.com>
2020-09-07 11:52:16 +0200

    general update

--- singularityContainer/Rpkgs/installRpkgs.R
+++ singularityContainer/Rpkgs/installRpkgs.R
@@ -1,10 +1,9 @@
 pth <- "/opt/miniCRAN"
 
-allPkgs <- read.table("/opt/Rpkgs.tsv",header=T)
+allPkgs <- read.table("/opt/Rpkgs.tsv",header=T,stringsAsFactors=F)
 alreadyInstalled <- row.names(installed.packages())
 manualInstallPkgs <- c("bisoreg","XML",alreadyInstalled)
 allPkgsAuto <- allPkgs[ ! allPkgs$Package %in% manualInstallPkgs, ]
-allPkgsAuto$Package <- factor(allPkgsAuto$Package, levels=allPkgsAuto$Package)
 
 #XML is needed by other pkgs like karyoplote, so you need to install first
 #install.packages("/opt/miniCRAN/src/contrib/XML_3.99-0.3.tar.gz", repos = NULL, type="source") #now it sould be fixed by addOldPackage()
@@ -14,4 +13,3 @@ install.packages(allPkgsAuto$Package, repos = paste0("file:///", pth), type = "s
 #first you need to install all the pkgs dependencies before installing bisoreg
 #since you add it manually its dependencies are not defined, so you need to install it separatelly at the end
 install.packages("/opt/miniCRAN/src/contrib/bisoreg_1.5.tar.gz", repos = NULL, type="source")
-
