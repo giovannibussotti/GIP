f4502684ee7c6aca1498e9bcaaabaf9e2a9f43c0
Giovanni Bussotti <giovannibussotti@gmail.com>
2020-10-22 16:39:46 +0200

    general update

--- singularityContainer/gipScripts/sampleComparison.sh
+++ singularityContainer/gipScripts/sampleComparison.sh
@@ -20,8 +20,19 @@ OPTS=${ALLIN/$MODE/}
 #SAMPLES=${ARGfound[0]}
 
 function printHelp {
+  echo ""
   echo "gitools modules:"
-  echo "karyotype"
+  echo ""
+  echo -e " karyotype       Compare the chromosome sequencing coverage distributions"
+  echo -e " binCNV          Compare bin sequencing coverage in 2 samples"
+  echo -e " geCNV           Compare gene sequencing coverage in 2 samples"
+  echo -e " ternary         Compare gene sequencing coverage in 3 samples"
+  echo -e " ternaryBin      Compare bin sequencing coverage in 3 samples"
+  echo -e " SNV             Compare SNVs in multiple samples"
+  echo -e " binDensity      Density plot of bin sequencing coverage of many samples"
+  echo -e " geInteraction   Detect CNV genes in many samples and produce correlation-based networks"
+  echo -e " genomeDistance  Compare samples genomic distance"
+  echo ""
 }
 
 if [ -z $MODE ]; then
@@ -31,47 +42,47 @@ fi
 
 if [ $MODE == "karyotype" ]; then
     CMD="Rscript /bin/karyotype $OPTS"
-    echo executing $CMD
+    #echo executing $CMD
     $CMD
 
 elif [ $MODE == "binCNV" ]; then
     CMD="Rscript /bin/binCNV $OPTS"
-    echo executing $CMD
+    #echo executing $CMD
     $CMD
 
 elif [ $MODE == "geCNV" ]; then
     CMD="Rscript /bin/geCNV $OPTS"
-    echo executing $CMD
+    #echo executing $CMD
     $CMD
 
 elif [ $MODE == "ternary" ]; then
     CMD="Rscript /bin/ternary $OPTS"
-    echo executing $CMD
+    #echo executing $CMD
     $CMD
 
 elif [ $MODE == "ternaryBin" ]; then
     CMD="Rscript /bin/ternaryBin $OPTS"
-    echo executing $CMD
+    #echo executing $CMD
     $CMD
 
 elif [ $MODE == "SNV" ]; then
     CMD="Rscript /bin/SNV $OPTS"
-    echo executing $CMD
+    #echo executing $CMD
     $CMD
 
 elif [ $MODE == "binDensity" ]; then
     CMD="Rscript /bin/binDensity $OPTS"
-    echo executing $CMD
+    #echo executing $CMD
     $CMD
 
 elif [ $MODE == "geInteraction" ]; then
     CMD="Rscript /bin/geInteraction $OPTS"
-    echo executing $CMD
+    #echo executing $CMD
     $CMD
 
 elif [ $MODE == "genomeDistance" ]; then
     CMD="Rscript /bin/genomeDistance $OPTS"
-    echo executing $CMD
+    #echo executing $CMD
     $CMD
 elif [ $MODE == "-h" ] || [ $MODE == "--help" ]; then
     printHelp
