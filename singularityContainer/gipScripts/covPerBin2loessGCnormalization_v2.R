0b90f89b9d15c53af692a084b1128327b505719c
Giovanni Bussotti <giovannibussotti@gmail.com>
2020-10-28 17:33:04 +0100

    general update

--- singularityContainer/gipScripts/covPerBin2loessGCnormalization_v2.R
+++ singularityContainer/gipScripts/covPerBin2loessGCnormalization_v2.R
@@ -6,6 +6,7 @@ parser$add_argument("--ASSEMBLY" , help="genome reference [default %(default)s]"
 parser$add_argument("--DIR" , help="dir with the covPerBin.gz file [default %(default)s]" )
 parser$add_argument("--SAMPLE" , help="sample name [default %(default)s]")
 parser$add_argument("--outName" , help="out name [default %(default)s]")
+parser$add_argument("--nuc" , help="bedtools nuc output (of an initial bed3 format file) matching the covPerBin file [default %(default)s]")
 parser$add_argument("--ylim" , type="integer" , help="[default %(default)s]" , default="10")
 parser$add_argument("--sampling" , type="integer" , help="sampling size. If 0 the entire (non-NA) dataset is considered [default %(default)s]" , default="0")
 parser$add_argument("--debug"  , action="store_true" , help="dump session and quit [default %(default)s]" , default=FALSE)
@@ -15,32 +16,16 @@ for (n in names(args)){if(args[[n]][1] == "NA"){args[[n]] <- NA}  }
 for (n in names(args)){assign(n,args[[n]]) }
 if(debug){library(session);save.session("session_DEBUG");quit()}
 
-library(Biostrings)
+#library(Biostrings)
 library(bisoreg)
-
 
 ##############################################
 #extract sequence of each bin and measure %GC#
 ##############################################
-refAssembly    <- readDNAStringSet(ASSEMBLY)
-df <- read.table(paste0(DIR,"/",SAMPLE,".covPerBin.gz"),header=T,stringsAsFactors=F,sep="\t")
+df    <- read.table(paste0(DIR,"/",SAMPLE,".covPerBin.gz"),header=T,stringsAsFactors=F,sep="\t")
 df$chromosome <- as.character(df$chromosome)
-seqs <- NULL
-CorGfreq <- NULL
-for (i in 1:length(df[,1])){
-        chr=df[i,"chromosome"]
-        start=df[i,"start"]
-        end=df[i,"end"]
-        seq <- subseq(refAssembly[[chr]],start, end)
-        af <- alphabetFrequency(seq)
-        freq <- (af[["C"]] + af[["G"]]) / (sum(af) - af[["N"]])
-        seq <- toString(seq)
-        rbind(seqs,seq) -> seqs
-        rbind(CorGfreq,freq) -> CorGfreq
-}
-#df$seqs    <- seqs
-df$CorGfreq <- CorGfreq
-df$CorGfreq <- as.vector(df$CorGfreq)
+df$CorGfreq   <- read.table(nuc , header=F , stringsAsFactors=F , sep="\t")[,5]
+
 
 #############################################
 #loess fitting then mean coverage correction#
@@ -72,13 +57,18 @@ df$lPredictAfterCorrection <- predict(lAfterCorrection, df$CorGfreq)
 ###################################################
 pdf(paste0(outName,".gcLnorm.covPerBin.pdf"))
 plotSmooth <- function (cc,N,mc,Y) {
-        smoothScatter(cc$CorGfreq , cc[[Y]], ylim=c(-3,ylim) , xlim=c(0.2 , 0.8) , xlab="%GC",ylab="coverage",main=N)
-        legend("topleft", c( paste("R=", round(cor(x=cc[[Y]] , y=cc$CorGfreq),2)) , "linear model" , "loess" ) , lty=c(0,2,1), col=c("","black","red") , bty="n" )
-	f <- paste(Y , "~ CorGfreq")
-        l = lm(f , data=cc)
-        abline(l,lty=2,col="black",lwd=2)
-        grid()
-        lines(cc$CorGfreq[order(cc$CorGfreq)], mc[order(cc$CorGfreq)],col="red")
+  #bandwidth has the same unit as data
+  #so if the range is 1-100 bandwidth 2 will smooth over 2%-wide regions
+  #the code below measures the range in each dimension and take the 2%
+  bwx <- ((max(cc$CorGfreq) - min(cc$CorGfreq)) /100) *2
+  bwy <- ((max(cc[[Y]]) - min(cc[[Y]])) /100) *2
+  smoothScatter(cc$CorGfreq , cc[[Y]], ylim=c(-3,ylim) , xlim=c(0.2 , 0.8) , xlab="%GC",ylab="coverage",main=N, bandwidth=c(bwx,bwy))
+  legend("topleft", c( paste("R=", round(cor(x=cc[[Y]] , y=cc$CorGfreq),2)) , "linear model" , "loess" ) , lty=c(0,2,1), col=c("","black","red") , bty="n" )
+  f <- paste(Y , "~ CorGfreq")
+  l <- lm(f , data=cc)
+  abline(l,lty=2,col="black",lwd=2)
+  grid()
+  lines(cc$CorGfreq[order(cc$CorGfreq)], mc[order(cc$CorGfreq)],col="red")
 }
 par(mfrow=c(1,2))
 cc <- df[complete.cases(df), ]
@@ -106,4 +96,3 @@ is.num     <- sapply(covPerBin, is.numeric)
 covPerBin[is.num] <- lapply(covPerBin[is.num], round, 3)
 write.table(x=covPerBin,append=F,col.names=T,row.names=F,sep="\t",quote=F,file=paste0(outName,".gcLnorm.covPerBin"))
 system(paste0("gzip " ,outName,".gcLnorm.covPerBin"))
-
